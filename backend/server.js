ad6dee21198be5f513ad161eadcbbf2150ad5148
hassan-mehboob <hassanmehboob691@gmail.com>
2022-02-15 11:03:34 +0500

    connecting to a database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from "express"
 import app from "./app.js"
+import connectDatabase from "./config/connectDatebase.js"
 import dotenv from "dotenv"
 
 // Config
 dotenv.config({ path: "backend/config/config.env" })
-
+connectDatabase()
 const port = process.env.PORT
 app.listen(port, () => console.log(`your server is listen on port ${port}`))
