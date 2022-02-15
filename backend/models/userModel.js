1797db92c0308f96ee5d356c9d313698e5c76c7f
hassan-mehboob <hassanmehboob691@gmail.com>
2022-02-15 17:00:49 +0500

    updating userSchema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -18,9 +18,15 @@ const userSchema = mongoose.Schema({
     type: String,
     requied: true,
   },
-  roole: {
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {
     type: String,
-    default: user,
+    default: "User",
   },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
